7a2122e68eb50adce598ac0379e8bce614aad350
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2025-06-06 00:06:49 +0300

    Custom Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 
+import { useState } from 'react'
 import './App.css'
 import { ControlledCheckbox, ControlledInput, ControlledInputWithFixedValue, ControlledSelect, GetValueOfUncontrolledInputByButtonPress, TracckValueOfUncontrolledInput, UncontrolledInput } from './input'
+import { Select } from './Select'
+
+type SelectItemProps = {
+  title: string
+  value: string
+}
 
 function App() {
+
+  const items: SelectItemProps[]  = [
+    {value: '1', title: 'firstItem'},
+    {value: '2', title: 'secondtItem'},
+    {value: '3', title: 'thirdItem'},
+    {value: '4', title: 'foursItem'},
+    {value: '5', title: 'fivesItem'},
+  ]
+
+  const [value, setValue] = useState<string>('1')
   
+ 
 
   return (
     <>
@@ -14,6 +32,7 @@ function App() {
       <ControlledInput/>
       <ControlledCheckbox/>
       <ControlledSelect/>
+      <Select onChange={(findedValue) => setValue(findedValue)} items={items} value={value}/>
     </>
   )
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,18 +1,73 @@
+import { useState } from 'react'
+import styles from './Select.module.css'
 
-type SelectProps = {
-    value: any
-    onClick: (value: any) => void
+type SelectItemProps = {
+    title: string
+    value: string
 }
 
-export const Select = ({value, onClick}: SelectProps) => {
+
+type SelectProps = {
+    value: string
+    onChange: (value: string) => void
+    items: SelectItemProps[]
+}
+
+export const Select = ({value, onChange, items}: SelectProps) => {
+
+    const [active, setActive] = useState<boolean>(false)
+    const selectedItem = items.find(i => i.value === value)
+
+    const toggleItems = () => {
+        setActive(!active)
+    }
+    
+    const clickedItemHandler = (value: string) => {
+        onChange(value)
+        setActive(false)
+    }
+
+    
     return (
-        <div>
-            <select>
-                    <option value={'1'}>one</option>
-                    <option value={'2'}>two</option>
-                    <option value={'3'}>three</option>
-                    <option value={'4'}>four</option>
-            </select>
+        <div className={styles.selectsSector}>
+
+            <div className={styles.selectSector}>
+                <span className={styles.title}>Custom select</span>
+                <h3 className={styles.selectValue}
+                    onClick={toggleItems}
+                >
+                    {selectedItem && selectedItem.title}
+                </h3>
+                    { active &&
+                        <div className={styles.selectItemsSection}>
+                        { items.map(item => (
+                                    <div 
+                                        onClick={() => clickedItemHandler(item.value)}
+                                        key={item.value}
+                                    >
+                                        {item.title}
+                                    </div>
+                                
+                            )
+                        )}
+                    </div>
+                    }
+                    
+            </div>
+
+            <div className={styles.selectSector}>
+                <span className={styles.title}>HTML select element</span>
+                <select>
+                    {items.map(item => {
+                        return (
+                            <option key={item.value}>
+                                {item.title}
+                            </option>
+                        )
+                    })}
+                </select>
+            </div>
+            
         </div>
     )
 }
